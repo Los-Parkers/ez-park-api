61c49aa275e9740cfe1c3a6ed39c72b608f892be
SgtGarro <marcelogarro137@gmail.com>
2024-06-09 09:11:23 -0500

    feat(TA-46): create ACL interface for Authentication bounded context

---
+++ Authentication/Interfaces/ACL/IUserContextFacade.cs
@@ -0,0 +1,13 @@
+﻿using ez_park_platform.Users.Domain.Model.Aggregates;
+
+namespace ez_park_platform.Authentication.Interfaces.ACL
+{
+    public interface IUserContextFacade
+    {
+
+        Task<User?> CreateUser(string Email, string Password, string FirstName, string LastName, string Dni, string Phone, DateTime DateOfBirth);
+        Task<User?> FecthUserByDni(string Eni);
+
+        Task<User?> FecthUserById(int Id);
+    }
+}
